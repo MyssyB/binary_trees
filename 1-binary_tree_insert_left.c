d2ecc132275a4919b8e46f2cf3d5c517cdd369d2
MyssyB <beulahagiande@gmail.com>
2024-06-04 06:10:04 -0700

    moore corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode = malloc(seizeof(binary_tree_t));
+
+	if (parent->left != NULL)
+        {
+                newNode->left = parent->left;
+                parent->left->parent = newNode;
+        }
+        parent->left = newNode;
+
+        return (newNode);
 
 	if (newNode == NULL)
 		return (NULL);
@@ -25,12 +34,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newNode->left = NULL;
 	newNode->right = NULL;
 
-	if (parent->left != NULL)
-	{
-		newNode->left = parent->left;
-		parent->left->parent = newNode;
-	}
-	parent->left = newNode;
-
-	return (newNode);
 }
