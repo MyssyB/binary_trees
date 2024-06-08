445a9f8d9b84ce664fb058e6ef89471aab81b1b2
MyssyB <beulahagiande@gmail.com>
2024-06-08 05:48:12 -0700

    betty error fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,23 +9,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
-		return (0);
-	else
 	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
-		
-		if (left_height > right_height)
-		{
-			return left_height + 1;
-		}
-		else
-		{
-			return right_height + 1;
-		}
+		return (0);
 	}
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height));
 }
